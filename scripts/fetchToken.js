4aa9edad52fed9163397c015ff140d808bddf70d
Marcel Stieber <marcel.stieber@finanzcheck.de>
2020-04-05 15:36:44 +0200

    chore: play around with figma api

--- scripts/fetchToken.js
+++ scripts/fetchToken.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
 
 // https://github.com/mikaelvesavuori/figmagic/tree/master/bin/functions
 // https://www.figma.com/file/K39TRbltDVcWFlpzw9r7Zh/Figmagic-%E2%80%94-Design-System-for-Tokens?node-id=2605%3A12
@@ -8,7 +10,11 @@ const axios = require('axios');
 const figmaKey = 'tpwhHRfoXMkzuFIYE9becy'; // TODO: put to env
 const figmaAPIToken = process.env.FIGME_PERSONAL_ACCESS_TOKEN;
 
-async function getFigmaObjTree(figmaApiKey, figmaId) {
+const PATH = path.resolve(__dirname, '../__temp__/figma.json');
+
+const storeFile = (fields) => fs.outputJson(PATH, fields);
+
+const getFigmaObjTree = async (figmaApiKey, figmaId) => {
   axios
     .get('https://api.figma.com/v1/files/' + figmaId, {
       headers: {
@@ -17,14 +23,57 @@ async function getFigmaObjTree(figmaApiKey, figmaId) {
       },
     })
     .then((res) => {
-      let result = res.data;
+      let result = res.data; // figmaTreeStructure
       // on result is the information about the last changed date -> compare with a cached one
       // console.log(result.document.children[0].children); // the frames
-      console.log(result.document.children[0].children[1]); // type
+      // console.log(result.document.children[0].children[1]); // type
+      // const x = fs.outputJson(path.resolve(PATH, `figma.json`), result);
+      console.log('start caching...');
+      storeFile(result);
+    })
+    .then(() => {
+      console.log('caching done');
     })
     .catch((err) => {
       console.log('errors');
       console.log(err);
     });
-}
+};
+
 getFigmaObjTree(figmaAPIToken, figmaKey);
+
+/*
+// TODO: normalize ->
+document -> children(CANVAS) -> children(FRAME.name -> colors, text, etc.) -> children -> [{}]
+  id
+  name
+  characters -> (actual text)
+  styles -> { fill: styleId }
+  fills -> color -> rgb: 255 * value each
+
+  e.g. name= SPACING -> children[x].name = 'huge' -> absoluteBoundingBox.widht|height
+  FRAME-NAME === type of normilization -> child-NAME === styleName value === fill|absoluteBoundingBox|style|...
+}]
+
+//  styles -> [{ styleId -> type, name, no values }] ?? usable ??
+
+Goal:
+"font_small": {           -> FRAMAE.name + '_' + CHILD.name
+  "value": "12",          -> needs selector
+  "type": "size",         -> needs mapping
+  "category": "sizing",   -> needs mapping
+  "meta": {               -> ?? needed ?? how to use in Figma?
+    "unit": "px"
+  }
+}
+
+style from document example:
+"lineHeightPx": 18.75,
+"lineHeightPercent": 100,
+-> needs translation to proper CSS -> meta could be used
+
+Questions:
+- what are team cmoponents?
+- how to use styles sub-object?
+- what categories does Theo have? -> https://github.com/salesforce-ux/theo#supported-categories
+*/
