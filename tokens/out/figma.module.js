ce1f7e3548f13eacb4eca143c2694ab0d03b6704
Marcel Stieber <marcel.stieber@finanzcheck.de>
2020-04-11 11:00:42 +0200

    build: change scripts to use figma data

---
+++ tokens/out/figma.module.js
@@ -0,0 +1,17 @@
+export const colorBlack = "rgba(51, 51, 51, 1)";
+export const colorBlue1 = "rgba(47, 128, 237, 1)";
+export const colorBlue2 = "rgba(45, 156, 219, 1)";
+export const colorBlue3 = "rgba(86, 204, 242, 1)";
+export const colorOrange = "rgba(242, 153, 74, 1)";
+export const colorWhite = "rgba(255, 255, 255, 1)";
+export const colorYellow = "rgba(242, 201, 76, 1)";
+export const colorGray1 = "rgba(79, 79, 79, 1)";
+export const colorNeon = "rgba(228, 255, 193, 1)";
+export const colorGray2 = "rgba(130, 130, 130, 1)";
+export const colorGray3 = "rgba(189, 189, 189, 1)";
+export const colorGray4 = "rgba(224, 224, 224, 1)";
+export const colorRed = "rgba(235, 87, 87, 1)";
+export const colorGray5 = "rgba(242, 242, 242, 1)";
+export const colorGreen1 = "rgba(33, 150, 83, 1)";
+export const colorGreen2 = "rgba(39, 174, 96, 1)";
+export const colorGreen3 = "rgba(111, 207, 151, 1)";
